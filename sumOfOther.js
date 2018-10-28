af8b15c79dfe82a0b7614259046c542e76a73c84
Vladislav Drobovich <vladdrobovich@gmail.com>
2018-10-28 20:05:12 +0300

    feat: add test.js

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,5 +1,3 @@
 module.exports=function sumOfOthers(array) {
 	return array.map(item=>array.reduce((acc,item)=>acc+item)-item);
 }
-
-// console.log(fn([2, 3, 4, 1]));
--- test.js
+++ test.js
@@ -2,3 +2,25 @@ const assert = require('assert');
 Object.freeze(assert);
 const sumOfOther = require('./sumOfOther');
 
+describe ("SumOfOther", function() {
+
+	it("should return new array, when each element at index 'i' is the sum of the remaining elements of the original array",
+	function () {
+		assert.deepEqual(sumOfOther([ 2, 3, 4, 1]),[8, 7, 6, 9]);
+	})
+
+	it("should return new array, when each element at index 'i' is the sum of the remaining elements of the original array",
+	function () {
+		assert.deepEqual(sumOfOther([ -2, -4, 1, 2, -4, 2, 3]),[ 0, 2, -3, -4, 2, -4, -5 ]);
+	})
+
+	it("should return new array, when each element at index 'i' is the sum of the remaining elements of the original array",
+	function () {
+		assert.deepEqual(sumOfOther([]),[]);
+	})
+
+	it("should return new array, when each element at index 'i' is the sum of the remaining elements of the original array",
+	function () {
+		assert.deepEqual(sumOfOther([-1,-2,-3,-4]),[-9,-8,-7,-6]);
+	})
+});
