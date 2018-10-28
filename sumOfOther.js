832db834955e1ad41bd3f7e7b11d394fb27f4d04
Vladislav Drobovich <vladdrobovich@gmail.com>
2018-10-28 19:26:44 +0300

    feat: add mocha test

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,5 +1,5 @@
-function fn(array) {
+module.exports=function sumOfOthers(array) {
 	return array.map(item=>array.reduce((acc,item)=>acc+item)-item);
 }
 
-console.log(fn([2, 3, 4, 1]));
+// console.log(fn([2, 3, 4, 1]));
---
+++ test.js
@@ -0,0 +1,4 @@
+const assert = require('assert');
+Object.freeze(assert);
+const sumOfOther = require('./sumOfOther');
+
